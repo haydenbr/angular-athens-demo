f54e6d5aec0fce314fbec5407ad0bac2c3a7e766
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2019-01-03 22:11:22 -0500

    make separate gulp tasks to watch and not watch templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 ﻿var gulp = require('gulp');
 var plug = require('gulp-load-plugins')();
 
-/**
- * Create $templateCache from the html templates
- * @return {Stream}
- */
+function generateTemplateCache() {
+  gulp
+    .src('./src/app/**/*.html')
+    .pipe(plug.htmlmin({
+        collapseWhitespace: true
+    }))
+    .pipe(plug.angularTemplatecache('templates.ts', {
+        module: 'ngaApp.core',
+        standalone: false,
+        root: 'app/'
+    }))
+    .pipe(gulp.dest('./src/app/'))
+}
+
 gulp.task('templatecache', function () {
   plug.watch('./src/app/**/*.html', { ignoreInitial: false }, function(events, done) {
-    gulp
-        .src('./src/app/**/*.html')
-        .pipe(plug.htmlmin({
-            collapseWhitespace: true
-        }))
-        .pipe(plug.angularTemplatecache('templates.js', {
-            module: 'ngaApp.core',
-            standalone: false,
-            root: 'app/'
-        }))
-        .pipe(gulp.dest('./src/app/'))
+    generateTemplateCache();
   });
+});
+
+gulp.task('templatecache:build', function () {
+  generateTemplateCache();
 });
